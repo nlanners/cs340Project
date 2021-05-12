f3be64ea4c6e91b32bd0099d76266791056351b5
nlanners <lannersn@oregonstate.edu>
2021-05-11 18:45:09 -0600

    reformatted with ejs updated file structure waiting for check before merge

--- main.js
+++ main.js
@@ -4,84 +4,87 @@ var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
 var path = require('path');
+var ejs = require('express-ejs-layouts');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json({extended:false}));
 app.use(express.static('public'));
+app.use(ejs);
 
 
 app.set('port', 65535);
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res, next){
-    res.sendFile('index.html');
+    res.render('index.ejs');
 });
 
 app.get('/characters', function(req, res, next) {
-    res.sendFile(path.join(__dirname, '/public', 'characters.html'));
+    res.render('characters.ejs');
 })
 
 app.get('/addRemoveCharacters', function(req, res, next) {
-    res.sendFile(path.join(__dirname, '/public', 'addRemoveCharacters.html'))
+    res.render('addRemoveCharacters.ejs')
 })
 
 app.get('/alterCharacters', function(req, res, next) {
-    res.sendFile(path.join(__dirname, '/public', 'alterCharacters.html'));
+    res.render('alterCharacters.ejs');
 })
 
 app.get('/items', function(req, res, next) {
-    res.sendFile(path.join(__dirname, '/public', 'items.html'));
+    res.render('items.ejs');
 })
 
 app.get('/addRemoveItems', function(req, res, next) {
-    res.sendFile(path.join(__dirname, '/public', 'addRemoveItems.html'));
+    res.render('addRemoveItems.ejs');
 })
 
 app.get('/alterItems', function(req, res, next) {
-    res.sendFile(path.join(__dirname, '/public', 'alterItems.html'));
+    res.render('alterItems.ejs');
 })
 
 app.get('/characterItems', function(req, res, next) {
-    res.sendFile(path.join(__dirname, '/public', 'characterItems.html'));
+    res.render('characterItems.ejs');
 })
 
 app.get('/spells', function(req, res, next) {
-    res.sendFile(path.join(__dirname, '/public', 'spells.html'));
+    res.render('spells.ejs');
 })
 
 app.get('/addRemoveSpells', function(req, res, next) {
-    res.sendFile(path.join(__dirname, '/public', 'addRemoveSpells.html'));
+    res.render('addRemoveSpells.ejs');
 })
 
 app.get('/alterSpells', function(req, res, next) {
-    res.sendFile(path.join(__dirname, '/public', 'alterSpells.html'));
+    res.render('alterSpells.ejs');
 })
 
 app.get('/enemies', function(req, res, next) {
-    res.sendFile(path.join(__dirname, '/public', 'enemies.html'));
+    res.render('enemies.ejs');
 })
 
 app.get('/addRemoveEnemies', function(req, res, next) {
-    res.sendFile(path.join(__dirname, '/public', 'addRemoveEnemies.html'));
+    res.render('addRemoveEnemies.ejs');
 })
 
 app.get('/alterEnemies', function(req, res, next) {
-    res.sendFile(path.join(__dirname, '/public', 'alterEnemies.html'));
+    res.render('alterEnemies.ejs');
 })
 
 app.get('/regions', function(req, res, next) {
-    res.sendFile(path.join(__dirname, '/public', 'regions.html'));
+    res.render('regions.ejs');
 })
 
 app.get('/addRemoveRegions', function(req, res, next) {
-    res.sendFile(path.join(__dirname, '/public', 'addRemoveRegions.html'));
+    res.render('addRemoveRegions.ejs');
 })
 
 app.get('/alterRegions', function(req, res, next) {
-    res.sendFile(path.join(__dirname, '/public', 'alterRegions.html'));
+    res.render('alterRegions.ejs');
 })
 
 app.get('/regionEnemies', function (req, res, next) {
-    res.sendFile(path.join(__dirname, '/public', 'RegionEnemies.html'))
+    res.render('RegionEnemies.ejs');
 })
 
 app.listen(app.get('port'), function(){
