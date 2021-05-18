8f2ead43a2623fa037b54c542c2bb326dde0f94d
nlanners <nicholas.lanners@gmail.com>
2021-05-18 17:28:25 -0600

    continued work on fetch

--- public/js/characterItemsSearch.js
+++ public/js/characterItemsSearch.js
@@ -2,12 +2,18 @@ document.addEventListener('DOMContentLoaded', bindButtons);
 
 function bindButtons() {
     document.getElementById('charSearch').addEventListener('click', function(event) {
+        var payload = { thing: 'happened' }
         fetch('/characterItems?action=charSearch', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify()
+            body: JSON.stringify(payload)
         }).then(response=>response.json())
+            .then( data => {console.log('Success: ', data);
+        })
+            .catch((error) => {
+                console.error('Error: ', error);
+            });
     })
 }
